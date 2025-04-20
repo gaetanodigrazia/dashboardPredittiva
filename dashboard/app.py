f5e05ef86ef49182dc125516f1cd8b4c60cc420d
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2025-04-20 15:03:12 +0200

    Added garage

--- dashboard/app.py
+++ dashboard/app.py
@@ -17,10 +17,7 @@ def input_form():
     data['GrLivArea'] = data['1stFlrSF'] + data['2ndFlrSF']
     st.markdown(f"📐 GrLivArea calcolata: `{data['GrLivArea']} m²`")
 
-    data['GarageCars'] = st.slider("Numero posti garage", 0, 5, 2)
-    garage_area_min = data['GarageCars'] * 12
-    data['GarageArea'] = st.slider("Garage Area", min_value=garage_area_min, max_value=1500, value=max(garage_area_min, 500))
-    st.markdown(f"ℹ️ Minimo GarageArea: {garage_area_min} m²")
+
 
     data['WoodDeckSF'] = st.slider("Wood Deck SF", 0, 1000, 200)
     data['OpenPorchSF'] = st.slider("Open Porch SF", 0, 1000, 100)
@@ -31,7 +28,7 @@ def input_form():
     data['OverallQual'] = st.selectbox("Overall Quality", list(range(1, 11)), index=5)
     data['YearBuilt'] = st.slider("Year Built", 1870, 2023, 2000)
     data['YearRemodAdd'] = st.slider("Year Remodeled", 1950, 2023, 2005)
-    data['GarageYrBlt'] = st.slider("Garage Year Built", 1900, 2023, 2000)
+
     data['MasVnrArea'] = st.slider("Masonry Veneer Area", 0, 1000, 100)
     data['BsmtFinSF1'] = st.slider("Basement Fin SF 1", 0, 2000, 500)
     data['BsmtFinSF2'] = st.slider("Basement Fin SF 2", 0, 2000, 200)
@@ -48,7 +45,7 @@ def input_form():
     data['TotRmsAbvGrd'] = st.slider("Total Rooms Above Ground", 1, 15, 6)
 
     st.subheader("🔥 Extra")
-    data['Fireplaces'] = st.slider("Number of Fireplaces", 0, 5, 1)
+
     data['MoSold'] = st.slider("Month Sold", 1, 12, 6)
     data['YrSold'] = st.slider("Year Sold", 2006, 2010, 2008)
 
@@ -56,9 +53,27 @@ def input_form():
     data['Multifloor'] = st.checkbox("Multifloor", value=True)
     data['IsNew'] = st.checkbox("Is New", value=False)
     data['has2ndfloor'] = st.checkbox("Has Second Floor", value=True)
-    data['hasgarage'] = st.checkbox("Has Garage", value=True)
     data['hasbsmt'] = st.checkbox("Has Basement", value=True)
-    data['hasfireplace'] = st.checkbox("Has Fireplace", value=True)
+
+    st.subheader("🚗 Garage")
+    data['hasgarage'] = st.checkbox("Has Garage", value=False)
+    if data['hasgarage']:
+        data['GarageCars'] = st.slider("Numero posti garage", 0, 5, 2)
+        garage_area_min = data['GarageCars'] * 12
+        data['GarageArea'] = st.slider("Garage Area", min_value=garage_area_min, max_value=1500, value=max(garage_area_min, garage_area_min))
+        st.markdown(f"ℹ️ Minimo GarageArea: {garage_area_min} m²")
+        data['GarageFinish'] = st.selectbox("Garage Finish", list(range(1, 3)), index=0)
+        data['GarageQual'] = st.selectbox("Garage Quality", list(range(1, 5)), index=0)
+        data['GarageCond'] = st.selectbox("Garage Condition", list(range(1, 5)), index=0)
+        data['GarageYrBlt'] = st.slider("Garage Year Built", 1900, 2023, 2000)
+    else:
+        data['GarageCars'] = 0
+        data['GarageArea'] = 0
+        data['GarageFinish'] = 0
+        data['GarageQual'] = 0
+        data['GarageCond'] = 0
+        data['GarageYrBlt'] = 0
+    
 
     total_sf_min = data['GrLivArea'] + data['TotalBsmtSF']
     data['TotalSF'] = st.slider("Total SF", min_value=total_sf_min, max_value=6000, value=total_sf_min)
@@ -82,6 +97,15 @@ def input_form():
         data['PoolArea'] = 0
         data['PoolQC'] = 0
 
+    st.subheader("🔥 Caminetto")
+    data['hasfireplace'] = st.checkbox("Has Fireplace", value=False)
+    if data['hasfireplace']:
+        data['Fireplaces'] = st.slider("Number of Fireplaces", 1, 5, 1)
+        data['FireplaceQu'] = st.selectbox("Fireplace Quality", list(range(1, 5)), index=0)
+    else:
+        data['Fireplaces'] = 0
+        data['FireplaceQu'] = 0
+
     st.subheader("🏷️ Categorie codificate")
     data['Feature1'] = st.selectbox("Feature 1", list(range(0, 10)), index=1)
     data['Feature2'] = st.selectbox("Feature 2", list(range(0, 10)), index=2)
@@ -92,10 +116,8 @@ def input_form():
     data['RoofStyle'] = st.selectbox("Roof Style", list(range(0, 6)), index=0)
     data['RoofMatl'] = st.selectbox("Roof Material", list(range(0, 8)), index=0)
     data['BsmtExposure'] = st.selectbox("Bsmt Exposure", list(range(0, 4)), index=0)
-    data['FireplaceQu'] = st.selectbox("Fireplace Quality", list(range(0, 5)), index=0)
-    data['GarageFinish'] = st.selectbox("Garage Finish", list(range(0, 3)), index=0)
-    data['GarageQual'] = st.selectbox("Garage Quality", list(range(0, 5)), index=0)
-    data['GarageCond'] = st.selectbox("Garage Condition", list(range(0, 5)), index=0)
+
+
     data['Fence'] = st.selectbox("Fence", list(range(0, 5)), index=0)
     data['MiscFeature'] = st.selectbox("Misc Feature", list(range(0, 5)), index=0)
     data['Alley'] = st.selectbox("Alley", list(range(0, 3)), index=0)
